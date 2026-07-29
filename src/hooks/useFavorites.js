7805e53d3d37b7c25a651f1529210e4ec5dc43e2
yafi <yafigian3@gmail.com>
2026-07-29 07:54:07 -0700

    Use environment variables for backend API

--- src/hooks/useFavorites.js
+++ src/hooks/useFavorites.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from "react";
 
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function useFavorites() {
   const [favorites, setFavorites] = useState([]);
